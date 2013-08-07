2c247b5382e824cd73f32ddacb554436c37b81ae
Renat <ridrisov@fortess.net>
2013-08-07 11:33:20 +0700

    initial fixes

--- sisal.ir.js
+++ sisal.ir.js
@@ -1,34 +1,40 @@
 complexNode = {
 	addNode : function (node) {
+		if (!node) return;
+		
 		// If the node is complex it have to be disassembled
-		if (node instanceof virtualComplexNode) {
+		if (node.pureVirtual) { //node instanceof virtualComplexNode) { -- this typing is not working, using duck typing
 			var increment=this.nodes.length;
 			for (var i=0;i<node.nodes.length;i++)
-				this.nodes[this.nodes.length]=node.nodes[i];
+				this.nodes.push(node.nodes[i]);
 			// copy variables
 			for (var i=0;i<node.vars.length;i++) {
-				this.vars[this.vars.length]=new variable(node.vars[i].nodeFrom===-1?-1:node.vars[i].nodeFrom+increment, portFrom, 
-														node.vars[i].nodeTo===-1?-1:node.vars[i].nodeTo+increment, portTo);
+				this.vars.push(new variable(node.vars[i].nodeFrom===-1?-1:node.vars[i].nodeFrom+increment, portFrom, 
+														node.vars[i].nodeTo===-1?-1:node.vars[i].nodeTo+increment, portTo));
 			}
 		} else 
-			this.nodes[this.nodes.length]=node;
+			this.nodes.push(node);
 	}
 }
 
 function virtualComplexNode () {
 	this.nodes=[];
 	this.vars=[];
+	this.pureVirtual = true;
 }
 virtualComplexNode.prototype=complexNode;
 
-function variable(nodeFrom, portFrom, nodeTo, portTo) {
-	this.nodeFrom=nodeFrom;
-	this.protFrom=portFrom;
-	this.nodeTo=nodeTo;
-	this.portTo=portTo;
+function variable(nodeFrom, portFrom, nodeTo, portTo, dtype) {
+	this.nodeFrom = nodeFrom;
+	this.portFrom = portFrom;
+	this.nodeTo = nodeTo;
+	this.portTo = portTo;
+	this.dtype = dtype;
 }
 
 function functionNode(name) {
+	this.nodes=[];
+	this.vars=[];
 	this.name=name;
 }
 functionNode.prototype=complexNode;
@@ -38,7 +44,7 @@ function typedName(id, dtype) {
 	this.dtype=dtype;
 }
 
-irGenerator = {
+var irGenerator = {
 	parse : function (astNode, inputs, outputs, undefined) {
 		if (Object.prototype.toString.call(astNode) === '[object Array]') { // Parse as array of parsed instances
 			var first=irGenerator.parse(astNode[0], inputs, outputs);
@@ -62,7 +68,7 @@ irGenerator = {
 					var node=irGenerator.parse(astNode.expressions[i], fInputs, fOutputs[i]);
 					func.addNode(node);
 				}
-				// Connect SubNodes			
+				// Connect SubNodes	
 				return func;
 			case 'TypedIdList':
 				var smth=new virtualComplexNode();
@@ -73,7 +79,7 @@ irGenerator = {
 			case 'ArrayOf':
 				var smth=new virtualComplexNode();
 				smth.addNode(new typedName(null, astNode.dtype));
-				return smth;				
+				return smth;
 		}
 	}
 }
