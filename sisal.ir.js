dfb26eec532d0b29c203c78a8105544605f8e330
Renat <ridrisov@fortess.net>
2013-08-09 11:55:05 +0700

    initial type parsing

--- sisal.ir.js
+++ sisal.ir.js
@@ -8,33 +8,43 @@ complexNode = {
 			for (var i=0;i<node.nodes.length;i++)
 				this.nodes.push(node.nodes[i]);
 			// copy variables
-			for (var i=0;i<node.vars.length;i++) {
-				this.vars.push(new variable(node.vars[i].nodeFrom===-1?-1:node.vars[i].nodeFrom+increment, portFrom, 
-														node.vars[i].nodeTo===-1?-1:node.vars[i].nodeTo+increment, portTo));
+			for (var i=0;i<node.edges.length;i++) {
+				this.edges.push(new edge(node.edges[i].nodeFrom===-1?-1:node.edges[i].nodeFrom+increment, portFrom, 
+														node.edges[i].nodeTo===-1?-1:node.edges[i].nodeTo+increment, portTo));
 			}
 		} else 
 			this.nodes.push(node);
 	}
 }
 
+function simpleType(name) {
+	this.name = name;
+}
+
+function arrayType(subType) {
+	this.isArray = true;
+	this.subType = subType;
+}
+
 function virtualComplexNode () {
 	this.nodes=[];
-	this.vars=[];
+	this.edges=[];
 	this.pureVirtual = true;
 }
 virtualComplexNode.prototype=complexNode;
 
-function variable(nodeFrom, portFrom, nodeTo, portTo, dtype) {
+function edge(nodeFrom, portFrom, nodeTo, portTo) {
 	this.nodeFrom = nodeFrom;
 	this.portFrom = portFrom;
 	this.nodeTo = nodeTo;
 	this.portTo = portTo;
-	this.dtype = dtype;
 }
 
-function functionNode(name) {
+function functionNode(name, inPorts, outPorts) {
 	this.nodes=[];
-	this.vars=[];
+	this.edges=[];
+	this.inPorts = inPorts;
+	this.outPorts = outPorts;
 	this.name=name;
 }
 functionNode.prototype=complexNode;
@@ -44,45 +54,66 @@ function typedName(id, dtype) {
 	this.dtype=dtype;
 }
 
-var irGenerator = {
-	parse : function (astNode, inputs, outputs, undefined) {
+function irGen() {
+	var self = this;
+	this.parseType = function (astNode) {
 		if (Object.prototype.toString.call(astNode) === '[object Array]') { // Parse as array of parsed instances
-			var first=irGenerator.parse(astNode[0], inputs, outputs);
+			var tuple=[];
+			for (var i=0;i<astNode.length;i++) {
+				tuple.push(self.parseType(astNode[i]));
+			}
+			return tuple;
+		}
+		if (astNode.type==="TypedIdList") {
+			var dtype, tuple = [];
+			if (["integer", "real"].indexOf(astNode.dtype)!==-1) {
+				dtype = new simpleType(astNode.dtype);
+			} else dtype = self.parseType(astNode.dtype);
+			for (var i=0;i<astNode.ids.length;i++) {
+				tuple.push(new typedName(astNode.ids[i], dtype));
+			}
+			return tuple;
+		}
+		if (astNode.type==="ArrayOf") {
+			var dtype;
+			if (["integer", "real"].indexOf(astNode.dtype)!==-1) {
+				dtype = new simpleType(astNode.dtype);
+			} else dtype = self.parseType(astNode.dtype);
+			return new arrayType(dtype);
+		}
+	}
+	
+	this.parse = function (astNode, inputs, outputs, undefined) {
+		if (Object.prototype.toString.call(astNode) === '[object Array]') { // Parse as array of parsed instances
+			var first=self.parse(astNode[0], inputs, outputs);
 			for (var i=1;i<astNode.length;i++) {
-				first.addNode(irGenerator.parse(astNode[i], inputs, outputs));
+				first.addNode(self.parse(astNode[i], inputs, outputs));
 			}
 			return first;
 		}
 		switch (astNode.type) {
 			case "Function":
-				var func=new functionNode(astNode.name);
-				var fInputs=irGenerator.parse(astNode.params);
-				var fOutputs=irGenerator.parse(astNode.returns);
+				var fInputs=self.parseType(astNode.params);
+				var fOutputs=self.parseType(astNode.returns);
 				// Create SubNodes
 				
-				if (astNode.expressions.length!==fOutputs.nodes.length) {
+/*				if (astNode.expressions.length!==fOutputs.nodes.length) {
 					throw "Defined and implemented output mismatch for function " + astNode.name;
-				}
+				}*/
+
+				var func=new functionNode(astNode.name, fInputs, fOutputs);
 				
 				for (var i=0;i<astNode.expressions.length;i++) {
-					var node=irGenerator.parse(astNode.expressions[i], fInputs, fOutputs[i]);
+					var node=self.parse(astNode.expressions[i], fInputs, fOutputs[i]);
 					func.addNode(node);
 				}
 				// Connect SubNodes	
 				return func;
-			case 'TypedIdList':
-				var smth=new virtualComplexNode();
-				for (var i=0;i<astNode.ids.length;i++) {
-					smth.addNode(new typedName(astNode.ids[i], astNode.dtype));
-				}
-				return smth;
-			case 'ArrayOf':
-				var smth=new virtualComplexNode();
-				smth.addNode(new typedName(null, astNode.dtype));
-				return smth;
 		}
 	}
 }
+
+var irGenerator = new irGen();
 
 function sisalir(ast){ // constructs sisal IR from Abstract syntax tree
 	this.nodes=[];
