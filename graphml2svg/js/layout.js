cd23b014884129316aa99b6c6421463823cdc48f
d <d@sisal.org>
2014-06-23 02:08:20 +0700

    Prepare xslt for layout

--- graphml2svg/js/layout.js
+++ graphml2svg/js/layout.js
@@ -13,17 +13,18 @@
                       '<node id="n0" xx="10" yy="10" width="300" heigth="300">' +      
                         '<data key="type">function</data>' +	  
                         '<graph id="n0:" edgedefault="directed">' +		
-                          '<node id="n0::p0" cx="100" cy="3">' +	
-                            '<data key="type">integer</data>' +			
-                            '<data key="subPort">true</data>' +		
+                          '<node id="n0::p0" cx="100">' +	
+                            '<data key="type">integer</data>' +
+                            '<data key="subPort">true</data>' +
                           '</node>' +		
-                          '<node id="n0::p1" cx="200" cy="3">' +			
+                          '<node id="n0::p1" cx="200">' +			
                             '<data key="type">integer</data>' +			
                             '<data key="subPort">true</data>' +
                           '</node>' +		
-                          '<node id="n0::p2" cx="150" cy="303">' +
+                          '<node id="n0::p2" cx="150">' +
                             '<data key="type">integer</data>' +
    			                '<data key="subPort">true</data>' +
+   			                '<data key="output">true</data>' +
                           '</node>' +		
                           '<node id="n0::n0" xx="100" yy="100">' +
        			            '<data key="type">+</data>' +
@@ -37,11 +38,11 @@
                             '<port name="right" />' +	
                             '<port name="out" />' +
                           '</node>' +
-               		      '<edge source="n0::p0" target="n0::n0" targetport="left" path="M 100 0 L 114 100" />' +
-                          '<edge source="n0::p0" target="n0::n1" targetport="right" path="M 100 0 L 228 200" />' +		
-                          '<edge source="n0::p1" target="n0::n1" targetport="left" path="M 200 0 L 128 100" />' +		
-                          '<edge source="n0::n1" target="n0::n0" sourceport="out" targetport="right" path="M 120 140 L 214 200" />' +		
-                          '<edge source="n0::n0" target="p2" sourceport="out" path="M 220 240 L 150 300" />' +
+               		      '<edge source="n0::p0" target="n0::n0" targetport="left" path="M 100 0 L 117 100" />' +
+                          '<edge source="n0::p0" target="n0::n1" targetport="right" path="M 100 0 L 231 200" />' +		
+                          '<edge source="n0::p1" target="n0::n1" targetport="left" path="M 203 0 L 131 100" />' +		
+                          '<edge source="n0::n1" target="n0::n0" sourceport="out" targetport="right" path="M 123 143 L 217 200" />' +		
+                          '<edge source="n0::n0" target="p2" sourceport="out" path="M 223 243 L 150 303" />' +
                         '</graph>' +
                       '</node>' +
                     '</graph></graphml>';
