1616202bac3526757c86f43a079ec5bda4688353
Renat Idrisov <ren@ngs.ru>
2014-08-19 18:42:02 +0700

    minor updates

--- test/buster.js
+++ test/buster.js
@@ -4,7 +4,7 @@ config["My tests"] = {
     rootPath: "../",
     environment: "node", // or "browser"
     sources: [
-        "interpreter/*.js",
+ //       "interpreter/*.js",
     ],
     tests: [
         "test/*-test.js"
