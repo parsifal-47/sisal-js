a5b3d3b9fbb527913d2b6b9f006bb0625f7f2b36
Renat Idrisov <ren@ngs.ru>
2014-08-19 18:42:02 +0700

    minor updates

--- si.js
+++ si.js
@@ -1,1 +1,10 @@
-// Sisal interactive entry point to be done
+var repl = require("repl");
+
+function eval(cmd, context, filename, callback) {
+  callback(null, "Error: repl is not implemented");
+}
+
+repl.start({
+  prompt: "si>",
+  eval: eval
+});
