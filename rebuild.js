e4ab72fa0d5a3c1bdb05a6e3dcc40ff11d7a8137
Renat Idrisov <ren@ngs.ru>
2014-08-21 19:21:05 +0700

    nodejs lex rebuild script

---
+++ rebuild.js
@@ -0,0 +1,15 @@
+/* nodejs only file which re-builds lex */
+
+var fs = require('fs');
+var PEG = require("pegjs");
+
+fs.readFile('lib/sisal.pegjs', 'utf8', function (err,data) {
+  if (err) return console.log(err);
+  var parser = PEG.buildParser(data, {output:"source"});
+  fs.writeFile('lib/lex.js', 'try { module = module || {}; } ' +
+			    'catch (e) { module = {}; } module.exports = ' + 
+				parser + ';', function (err) {
+	  if (err) return console.log(err);
+	  console.log('lex has been rebuilt');
+  });
+});
