85da8e3e728ee75b129210a71606892e306f89b2
Renat Idrisov <ren@ngs.ru>
2014-08-25 17:12:04 +0700

    first working repl version

--- si.js
+++ si.js
@@ -1,7 +1,14 @@
+require("./lib/lex.js");
+require("./lib/ir.nodes.js");
+require("./lib/ir.other.js");
+require("./lib/ir.gen.js");
+require("./lib/si.js");
 var repl = require("repl");
 
 function eval(cmd, context, filename, callback) {
-  callback(null, "Error: repl is not implemented");
+  var clean = cmd.substring(1, cmd.length -2); // removing extra brackets
+  var result = JSON.stringify(si.run(cmd));
+  callback(null, result);
 }
 
 repl.start({
