f2bec64d05cfbe79722457ad75111a6eb51503e6
Renat Idrisov <ren@ngs.ru>
2014-08-25 22:36:59 +0700

    examples restructure and update

--- test/lex-test.js
+++ test/lex-test.js
@@ -1,6 +1,24 @@
 var buster = require("buster");
 var assert = buster.referee.assert;
-//require("../lib/lex.js");
+var fs = require("fs");
+
+function checkDir(path) {
+    var exclude = ["older", "complex"];
+    var data = fs.readdirSync(path);
+    for (var i = 0; i<data.length; i++) {
+        if (fs.lstatSync(path+data[i]).isDirectory()) {
+            if (exclude.indexOf(data[i]) === -1) {
+                checkDir(path+data[i]+ "/");
+            }
+            continue;
+        }
+        console.log(path+data[i]);
+
+        var prog = fs.readFileSync(path+data[i]);
+        AST = lex.parse("" + prog);
+        assert.greater(AST.length, 0, "lex for ...");
+    }
+}
 
 buster.testCase("Lex", {
     "trivial lex test": function () {
@@ -14,14 +32,6 @@ buster.testCase("Lex", {
         assert.greater(AST.length, 0, "lex should reproduce non-empty program");
     },
     "check all programs in examples folder": function () {
-        var fs = require("fs");
-        var data = fs.readdirSync("examples/");
-        for (var i = 0; i<data.length; i++) {
-            if (fs.lstatSync("examples/"+data[i]).isDirectory()) continue;
-
-            var prog = fs.readFileSync("examples/"+data[i]);
-            AST = lex.parse("" + prog);
-            assert.greater(AST.length, 0, "lex for ...");
-        }       
+        checkDir("examples/");
     }
 });
